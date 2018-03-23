94d604dd5a1d4c508aad20c51a9576671ffe9fe8
Adam Touzani <adamtouzani@icloud.com>
2018-03-23 23:12:39 +0100

    fixed I-type header files and source files in Assembler code, added implementation of Bug.h and Bug.cpp

--- Simulator/header_files/Bug.h
+++ Simulator/header_files/Bug.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * File:   Bug.h
  */
 
@@ -6,7 +6,32 @@
 #define BUG_H
 
 class Bug{
-
+private:
+  tcolor color;
+  tstate state;
+  int prog_id;
+  tposition pos;
+  tdirection direction;
+  int resting;
+  int remaining_rest;
+  bool food;
+  bool dead;
+public:
+  Bug(tcolor i_col, int i_progid, int i_resting);
+  void start_resting();
+  bool rested();
+  int get_progid();
+  tcolor get_color();
+  tstate get_state();
+  void set_state(tstate new_state);
+  tdirection get_direction();
+  void set_direction(tdirection new_direction);
+  tposition get_position();
+  void set_position(tposition new_position);
+  bool get_food();
+  void set_food(bool new_food);
+  bool is_dead();
+  void kill();
 };
 
 #endif /* BUG_H */
