74238738dd2375e5c10d8c34cf4b0ba131495157
Adam Touzani <adamtouzani@icloud.com>
2018-04-09 23:55:33 +0200

    Added empty main.cpp (to handle console input)

---
+++ Assembler/source_files/main.cpp
@@ -0,0 +1,10 @@
+#include <stdio>
+#include <string>
+#include "../header_files/Program.h"
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+  //TODO: Add code to handle input from console
+  return 0;
+}
