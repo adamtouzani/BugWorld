984e601ae8fb72cbc1b1deb38fb6bbedae9a6026
Adam Touzani <adamtouzani@icloud.com>
2018-04-09 22:45:55 +0200

    Remove more unecessary files

--- Simulator/build/Debug/GNU-MacOSX/source_files/I_direction.o.d
+++
@@ -1,8 +0,0 @@
-build/Debug/GNU-MacOSX/source_files/I_direction.o: \
-  source_files/I_direction.cpp \
-  source_files/../header_files/Instruction.h \
-  source_files/../header_files/Bug.h
-
-source_files/../header_files/Instruction.h:
-
-source_files/../header_files/Bug.h:
