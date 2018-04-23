b46940494059a4fa4aca8208ee5f4e901272ca60
Smehta <mehta.sahil1997@gmail.com>
2018-04-23 17:49:11 +0200

    I_pickup changes

--- Simulator/source_files/I_pickup.cpp
+++ Simulator/source_files/I_pickup.cpp
@@ -12,9 +12,17 @@ using namespace std;
 
 I_pickup::I_pickup(World *w) : Instruction(w){}
 
-void I_pickup::execute(Bug b){
+void I_pickup::execute(Bug *b){
   if (!b.get_food()) {
-    Cell temp =
+    Cell *temp = w->getCell(b->get_position());
+    int food = temp->getFood();
+    if (food == 0) {
+      b->set_state(this->y);
+    } else {
+      temp->setFood(food-1);
+      b->set_food(true);
+      b->set_state(x);
+    }
   }
   return;
 };
